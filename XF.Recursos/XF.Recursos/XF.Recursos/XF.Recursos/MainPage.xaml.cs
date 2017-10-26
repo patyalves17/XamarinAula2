38aa9896813f3b9051b5899b330465b2411b01ef
patyalves17 <patriciaalves10@gmail.com>
2017-10-25 22:02:20 -0200

    aula 02

---
+++ XF.Recursos/XF.Recursos/XF.Recursos/XF.Recursos/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Recursos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSimples_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.SimplePage());
+        }
+
+        private async void btnGlobal_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.GeralView());
+        }
+
+        private async void btnDinamico_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.DinamicoView());
+        }
+
+        private async void btnTriggers_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.TriggersView());
+        }
+    }
+}
